57b9aa9ed166f6f90e8472a2e2cdd14609e3e4d4
ahnaf-khan-82 <ahnafabrarkhan14@gmail.com>
2025-09-26 21:02:08 -0400

    Add files via upload

---
+++ InClass Activity 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WindowEngine
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
